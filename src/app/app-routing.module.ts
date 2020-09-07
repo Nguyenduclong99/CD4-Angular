ad41f317aaeb5f0055f9938a9ac872868c802db4
Nguyenduclong99 <55075056+Nguyenduclong99@users.noreply.github.com>
2020-09-07 11:15:44 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NotFoundComponent} from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+     {path: '', loadChildren:() => import('./home/home.module').then(m => m.HomeModule)  },
+     {path: 'search', loadChildren:() => import('./search/search.module').then(m => m.SearchModule)  },
+     {path: 'product', loadChildren:() => import('./product/product.module').then(m => m.ProductModule)  },
+     {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { SlideComponent } from './home/slide/slide.component';
+import { ListItemComponent } from './product/list-item/list-item.component';
+import { DetailComponent } from './product/detail/detail.component';
+import { CartComponent } from './product/cart/cart.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    //SearchComponent,
+    //ProductComponent,
+    //SlideComponent,
+    //ListItemComponent,
+    //DetailComponent,
+    //CartComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
